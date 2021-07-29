174c32d959be34768a1654806f34b97edee6a174
Aleksandr <vilkovaleksandrstupino@gmail.com>
2021-07-30 06:51:01 +0900

    Added empty methodes for Part class

--- base/part.cpp
+++ base/part.cpp
@@ -4,3 +4,23 @@ Part::Part()
 {
 
 }
+
+void Part::SetMaterial(QString arg)
+{
+    material_mark = arg;
+}
+
+void Part::UpdateGeometricCenter()
+{
+
+}
+
+void Part::UpdateVolume()
+{
+
+}
+
+void Part::UpdateMassCenter()
+{
+
+}
